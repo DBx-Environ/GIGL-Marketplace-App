464c73e2ee73bdd0d68d34a813c5d17aa43993dc
David Baxter <david@baxterenvironmental.co.uk>
2025-07-10 17:56:18 +0100

    Attempt deploy after granting Cloud Functions Admin and Editor roles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, onAuthStateChanged, signOut, updateProfile, sendPasswordResetEmail } from 'firebase/auth';
 import { getFirestore, doc, setDoc, getDoc, collection, onSnapshot, serverTimestamp, addDoc, updateDoc, deleteDoc } from 'firebase/firestore';
 
-// --- Context for User and Firebase Instances ---
+// --- Context for User and Firebase Instances Hello! ---
 const AppContext = createContext();
 
 // Custom Message Box Component
